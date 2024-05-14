0f59c60aecc63dcfb7769cc3ba43250ec516f9a2
Ben Sandee <ben@orangebikelabs.com>
2024-05-14 12:06:25 -0500

    fix: update drizzle-kit to 0.21.1

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,11 @@ if (process.env.DATABASE_URL == null) {
   throw new Error("DATABASE_URL is not defined in the environment variables");
 }
 export default {
-  driver: "pg",
+  dialect: "postgresql",
   schema: "./src/lib/db/schema.ts",
   out: "./src/lib/db/migrations",
   verbose: true,
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 } satisfies Config;
