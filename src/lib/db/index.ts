fd6df30f9195284f72911100be2169f948c01cc1
Ben Sandee <ben@orangebikelabs.com>
2024-05-16 09:46:46 -0500

    chore: better diagnostics

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+
+if (process.env.DATABASE_URL === undefined) {
+  throw new Error("DATABASE_URL is not set in the environment variables");
+}
 // create database connection
-const connection = postgres(process.env.DATABASE_URL as string);
+const connection = postgres(process.env.DATABASE_URL);
 
 export const database = drizzle(connection);
