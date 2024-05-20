42e5153f1d7a10248e46adf2a10d9f06f67bb8d5
Ben Sandee <ben@orangebikelabs.com>
2024-05-20 12:19:06 -0500

    fix: enable jest path mapping

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "jest";
+import { pathsToModuleNameMapper } from "ts-jest";
 import nextJest from "next/jest.js";
+import { compilerOptions } from "./tsconfig.json";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -10,6 +12,9 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
+  moduleNameMapper: pathsToModuleNameMapper(
+    compilerOptions.paths /*, { prefix: '<rootDir>/' } */,
+  ),
   // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
