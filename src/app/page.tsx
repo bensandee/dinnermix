baa85256ae55366350da68f537486af2a65bd806
Ben Sandee <ben@orangebikelabs.com>
2024-10-29 11:57:22 -0500

    fix: use updated nexjs auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { useUser } from "@auth0/nextjs-auth0";
 import Link from "next/link";
 
 export default function Home() {
